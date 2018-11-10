51ff545c7f5b231321a14c82beb783ce222f717a
Amr Elshamy <ammohamed@icct.com>
2018-11-09 21:40:12 -0500

    add chat list item widget

---
+++ lib/src/components/tiles/chat_list_tile.dart
@@ -0,0 +1,38 @@
+import 'package:backpack/src/components/tiles/circle_avatar_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class ChatListTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final bool isOnline;
+
+  ChatListTile({Key key, this.title, this.subtitle, this.isOnline = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatarListTile(
+      title: title,
+      subtitle: subtitle,
+      trailing: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            '9:45AM',
+            style: Theme.of(context).textTheme.body2.copyWith(color: Colors.grey),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 7.0),
+            width: 10.0,
+            height: 10.0,
+            decoration: isOnline
+                ? BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.blue,
+                  )
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+}
